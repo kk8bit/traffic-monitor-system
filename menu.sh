8a53a303ee0ab38612dbd77b55a33bdf79adedde
kk8bit <guemu@qq.com>
2024-11-19 15:34:49 +0800

    v0.1.10菜单优化

--- menu.sh
+++ menu.sh
@@ -31,19 +31,19 @@ format_bytes() {
     printf "%.${precision}f %s\n" "$size" "${units[$i]}"
 }
 show_menu() {
-    echo -e "\n请选择一个选项:\n"
-    echo "1. 查看实时流量"
-    echo "2. 查看实时连接"
-    echo "3. 查看小时流量统计"
-    echo "4. 查看7日流量统计"
-    echo "5. 查看月度流量统计"
-    echo "6. 启动监控服务"
-    echo "7. 停止监控服务"
-    echo "8. 重启监控服务"
-    echo "9. 查看监控服务状态"
-    echo "10. 卸载流量监控系统"
+    echo -e "\n选择序号:\n"
+    echo "1. 实时流量"
+    echo "2. 实时连接"
+    echo "3. 小时流量"
+    echo "4. 7日流量"
+    echo "5. 月度流量"
+    echo "6. 启动服务"
+    echo "7. 停止服务"
+    echo "8. 重启服务"
+    echo "9. 服务状态"
+    echo "10. 卸载"
     echo "0. 退出"
-    echo -n "请输入选项 [0-10]: "
+    echo -n "输入序号 [0-10]: "
 }
 # 在脚本开始时显示logo和GitHub地址
 if [ -z "$first_run" ]; then
@@ -62,28 +62,28 @@ while true; do
             nload -u H -m ens5 ;;
         2) 
             iftop -i ens5 || {
-                echo -e "\e[33m注意：查看实时连接需要root权限。请使用sudo再次尝试。\e[0m"
+                echo -e "\e[33m注意：实时连接需要root权限。使用sudo再次尝试。\e[0m"
                 echo -e "\n按任意键继续..."
                 read -n 1 -s -r
                 continue
             } ;;
-        3) vnstat -i ens5 -h || echo "无法获取小时流量统计。请检查vnstat服务是否已安装并运行。" ;;
+        3) vnstat -i ens5 -h || echo "无法获取小时流量。检查vnstat服务是否已安装并运行。" ;;
         4)
             # 获取今日日期
             today=$(date +%Y-%m-%d)
             # 获取7天前的日期
             seven_days_ago=$(date -d "$today -6 days" +%Y-%m-%d)
-            # 使用vnstat查看7天内的流量统计
-            vnstat -i ens5 -d --begin "$seven_days_ago" --end "$today" || echo "无法获取7日流量统计。请检查vnstat服务是否已安装并运行。" ;;
+            # 使用vnstat7天内的流量
+            vnstat -i ens5 -d --begin "$seven_days_ago" --end "$today" || echo "无法获取7日流量。检查vnstat服务是否已安装并运行。" ;;
         5) 
-            vnstat -i ens5 -m || echo "无法获取月度流量统计。请检查vnstat服务是否已安装并运行。" ;;
-        6) sudo systemctl start monitor-traffic || echo "无法启动监控服务。" ;;
-        7) sudo systemctl stop monitor-traffic || echo "无法停止监控服务。" ;;
-        8) sudo systemctl restart monitor-traffic || echo "无法重启监控服务。" ;;
+            vnstat -i ens5 -m || echo "无法获取月度流量。检查vnstat服务是否已安装并运行。" ;;
+        6) sudo systemctl start monitor-traffic || echo "无法启动服务。" ;;
+        7) sudo systemctl stop monitor-traffic || echo "无法停止服务。" ;;
+        8) sudo systemctl restart monitor-traffic || echo "无法重启服务。" ;;
         9) sudo systemctl status monitor-traffic ;;
         10) sudo /usr/local/bin/uninstall.sh ;;
         0) echo "退出菜单"; exit 0 ;;
-        *) echo "无效选项，请重新选择" ;;
+        *) echo "无效选项，重新选择" ;;
     esac
     echo -e "\n按任意键继续..."
     read -n 1 -s -r
